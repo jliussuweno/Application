221ba6e4a995dd8240292c72b9c23df4366bfc08
Jlius Suweno <jliuss@live.com>
2020-11-20 16:58:04 +0700

    update terakhir
    Gak bisa di pake, bukan modular

--- Login/LoginViewController.h
+++ Login/LoginViewController.h
@@ -7,6 +7,7 @@
 
 #import <UIKit/UIKit.h>
 #import "RegisterViewController.h"
+#import "HomeViewController.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
--- Login/LoginViewController.m
+++ Login/LoginViewController.m
@@ -6,6 +6,10 @@
 //
 
 #import "LoginViewController.h"
+#import <Register/Register.h>
+#import "RegisterViewController.h"
+//#import <Home/Home.h>
+#import "HomeViewController.h"
 
 @interface LoginViewController ()
 
@@ -21,15 +25,45 @@
     NSString *password = _passwordTextField.text;
     NSLog(@"userID = %@", userID);
     NSLog(@"password = %@", password);
+    if ([userID  isEqual: @"admin"] && [password  isEqual: @"123"]) {
+        [self ShowAlert:@"Berhasil!"];
+//        NSString *frameworkDirPath = [[NSBundle mainBundle] privateFrameworksPath];
+//        NSString *frameworkBundlePath = [frameworkDirPath stringByAppendingPathComponent:@"Home.framework"];
+//        NSBundle *frameworkBundle = [NSBundle bundleWithPath:frameworkBundlePath];
+//        HomeViewController *homeViewController = [[HomeViewController alloc] initWithNibName:@"HomeViewController" bundle:frameworkBundle];
+//        homeViewController.modalPresentationStyle = UIModalPresentationOverFullScreen;
+//        homeViewController.modalTransitionStyle = UIModalTransitionStyleCoverVertical;
+//        [self presentViewController:homeViewController animated:YES completion:nil];
+    } else {
+        [self ShowAlert:@"Maaf, user ID dan password Anda salah, silahkan ulangi kembali."];
+    }
 }
 - (IBAction)registerPressed:(id)sender {
-    NSString *frameworkDirPath = [[NSBundle mainBundle] privateFrameworksPath];
-    NSString *frameworkBundlePath = [frameworkDirPath stringByAppendingPathComponent:@"Register.framework"];
-    NSBundle *frameworkBundle = [NSBundle bundleWithPath:frameworkBundlePath];
-//    RegisterViewController *registerViewController = [[RegisterViewController alloc] initWithNibName:@"RegisterViewController" bundle:frameworkBundle];
-//    LoginViewController *loginViewController = [[LoginViewController alloc] initWithNibName:@"LoginViewController" bundle:frameworkBundle];
+//    NSString *frameworkDirPath = [[NSBundle mainBundle] privateFrameworksPath];
+//    NSString *frameworkBundlePath = [frameworkDirPath stringByAppendingPathComponent:@"Register.framework"];
+//    NSBundle *frameworkBundle = [NSBundle bundleWithPath:frameworkBundlePath];
 //    RegisterViewController *registerViewController = [[RegisterViewController alloc] initWithNibName:@"RegisterViewController" bundle:frameworkBundle];
 //    registerViewController.modalPresentationStyle = UIModalPresentationOverFullScreen;
+//    registerViewController.modalTransitionStyle = UIModalTransitionStyleCoverVertical;
 //    [self presentViewController:registerViewController animated:YES completion:nil];
+}
+
+- (void) ShowAlert:(NSString *)Message {
+    UIAlertController * alert=[UIAlertController alertControllerWithTitle:nil
+                                                                  message:@""
+                                                           preferredStyle:UIAlertControllerStyleAlert];
+    UIView *firstSubview = alert.view.subviews.firstObject;
+    UIView *alertContentView = firstSubview.subviews.firstObject;
+    for (UIView *subSubView in alertContentView.subviews) {
+        subSubView.backgroundColor = [UIColor colorWithRed:100/255.0f green:0/255.0f blue:254/255.0f alpha:1.0f];
+    }
+    NSMutableAttributedString *AS = [[NSMutableAttributedString alloc] initWithString:Message];
+    [AS addAttribute: NSForegroundColorAttributeName value: [UIColor whiteColor] range: NSMakeRange(0,AS.length)];
+    [alert setValue:AS forKey:@"attributedTitle"];
+    [self presentViewController:alert animated:YES completion:nil];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [alert dismissViewControllerAnimated:YES completion:^{
+        }];
+    });
 }
 @end
